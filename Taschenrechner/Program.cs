1def3cef1d12e9ff41b84e6780bac6fcfb0e4711
Hermann Bernard <hermann.bernard@outlook.de>
2021-05-24 07:19:13 +0200

    Methode - HoleSummanden - implementiert

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -4,15 +4,22 @@ namespace Taschenrechner
 {
     class Program
     {
+        // METHODE DEFINIEREN in 7 Schritten
+        // 1. (optional) Modifizierer definieren
+        // 2. Datentyp des Rückgabewertes definieren
+        // 3. Methodennamen definieren
+        // 4. Runde Klammern an den Methodennamen anfügen
+        // 5. Überlegen welche Parameter benötigt werden (optional diese definieren)
+        // 6. Geschweifte Klammern einfügen
+        // 7. Methode implementieren (Anweisungen in den Methodenrumpf schreiben)
+
         static void Main(string[] args)
         {
             // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
 
             // Eingabe der beiden Summanden
-            Console.Write("Bitte gib den ersten Summanden ein: ");
-            string ersterSummand = Console.ReadLine();
-            Console.Write("Bitte gib den zweiten Summanden ein: ");
-            string zweiterSummand = Console.ReadLine();
+            string ersterSummand = HoleSummanden("Bitte gib den ersten Summanden ein: ");
+            string zweiterSummand = HoleSummanden("Bitte gib den zweiten Summanden ein: ");
 
             // Wandel Text in Gleitkommazahlen
             double ersterSummandAlsZahl = Convert.ToDouble(ersterSummand);
@@ -26,9 +33,18 @@ namespace Taschenrechner
             WarteAufBenutzereingabe();
         }
 
+        static string HoleSummanden(string ausgabeText)
+        {
+            Console.Write(ausgabeText);
+            string summand = Console.ReadLine();
+
+            return summand;
+        }
+
         static double Addiere(double ersterSummand, double zweiterSummand)
         {
             double summe = ersterSummand + zweiterSummand;
+
             return summe;
         }
 
