edf066fdbd19447c034bc0a848e4cd600cf51add
Hermann Bernard <hermann.bernard@outlook.de>
2021-05-29 07:27:35 +0200

    Klasse ConsoleView angelegt, und die Methoden für die  Aus- und Eingaben aus der Program.cs dorthin verschoben

---
+++ Taschenrechner/ConsoleView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class ConsoleView
+    {
+        public string HoleBenutzereingabe(string ausgabeText)
+        {
+            Console.Write(ausgabeText);
+            string zahl = Console.ReadLine();
+
+            return zahl;
+        }
+        public void ResultatAusgeben(double ergebnis, double Zahl2, string operand)
+        {
+            // Division durch 0 anmerken
+            if ((Zahl2 == 0) && (operand == "/"))
+            {
+                Console.WriteLine("Division durch 0 ist nicht definiert, Kollege!");
+            }
+            // Kein gültiger Rechenoperand anmerken
+            else if ((operand != "+") && (operand != "-") && (operand != "*") && (operand != "/"))
+            {
+                Console.WriteLine("Du hast keinen gültigen Rechenoperator eingegeben!");
+            }
+            else
+            {
+                Console.WriteLine("Das Resultat ist: {0}", ergebnis);
+            }
+        }
+    }
+}
--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -7,17 +7,13 @@ namespace Taschenrechner
 
         static void Main(string[] args)
         {
-            // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
-
-            
-            double resultat = 0;
-
+            ConsoleView view = new ConsoleView();
 
             // Eingabe der beiden Zahlen
-            string ersteZahlAlsString = HoleBenutzereingabe("Bitte gib die erste Zahl ein: ");
-            string zweiteZahlAlsString = HoleBenutzereingabe("Bitte gib die zweite Zahl ein: ");
+            string ersteZahlAlsString = view.HoleBenutzereingabe("Bitte gib die erste Zahl ein: ");
+            string zweiteZahlAlsString = view.HoleBenutzereingabe("Bitte gib die zweite Zahl ein: ");
             // Eingabe der Rechenoperation
-            string rechenoperator = HoleBenutzereingabe("Bitte gib einen Rechenoperator ein (+ oder - oder * oder /): ");
+            string rechenoperator = view.HoleBenutzereingabe("Bitte gib einen Rechenoperator ein (+ oder - oder * oder /): ");
             
             // Wandel Text in Gleitkommazahlen
             // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist
@@ -29,38 +25,12 @@ namespace Taschenrechner
             model.Berechne(ersteZahl, zweiteZahl, rechenoperator);
 
             // Ausgabe des Resultats
-            ResultatAusgeben(model.Resultat, zweiteZahl, rechenoperator);
+            view.ResultatAusgeben(model.Resultat, zweiteZahl, rechenoperator);
 
             Console.WriteLine();
-            HoleBenutzereingabe("Zum Beenden bitte - Return - drücken");
+            view.HoleBenutzereingabe("Zum Beenden bitte - Return - drücken");
         }
 
-        static string HoleBenutzereingabe(string ausgabeText)
-        {
-            Console.Write(ausgabeText);
-            string zahl = Console.ReadLine();
-
-            return zahl;
-        }
-                          
-
-        static void ResultatAusgeben(double ergebnis, double Zahl2, string operand)
-        {
-            // Division durch 0 anmerken
-            if ((Zahl2 == 0) && (operand == "/"))
-            {
-                Console.WriteLine("Division durch 0 ist nicht definiert, Kollege!");
-            }
-            // Kein gültiger Rechenoperand anmerken
-            else if ((operand != "+") && (operand != "-") && (operand != "*") && (operand != "/"))
-            {
-                Console.WriteLine("Du hast keinen gültigen Rechenoperator eingegeben!");
-            }
-            else
-            {
-            Console.WriteLine("Das Resultat ist: {0}", ergebnis);
-            }
-        }
 
  
     }
