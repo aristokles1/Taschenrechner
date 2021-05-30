3b5c9605fc887ba3c650d4904bd0ffae48ec4812
Hermann Bernard <hermann.bernard@outlook.de>
2021-05-30 06:40:22 +0200

    Klasse AnwendungsController.cs erstellt und sämliche Ausführungsanweisungen aus Program.cs dort hinein verlagert

---
+++ Taschenrechner/AnwendungsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Taschenrechner
+{
+    class AnwendungsController
+    {
+        private ConsoleView view;
+        private RechnerModel model;
+
+        public AnwendungsController(ConsoleView view, RechnerModel model)
+        {
+            this.view = view;
+            this.model = model;
+        }
+
+        public void Ausfuehren()
+        {
+            // Eingabe
+            string ersteZahlAlsString = view.HoleZahlVomBenutzer();
+            string rechenoperator = view.HoleOperatorVomBenutzer();
+            string zweiteZahlAlsString = view.HoleZahlVomBenutzer();
+
+            // Wandel Text in Gleitkommazahlen
+            // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist
+            double ersteZahl = Convert.ToDouble(ersteZahlAlsString);
+            double zweiteZahl = Convert.ToDouble(zweiteZahlAlsString);
+
+            // Berechnung ausführen
+            model.Berechne(ersteZahl, zweiteZahl, rechenoperator);
+
+            // Ausgabe
+            view.GibResultatAus(zweiteZahl, rechenoperator);
+            Console.WriteLine();
+            view.WarteAufEndeDurchBenutzer();
+
+        }
+    }
+}
--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -9,24 +9,10 @@ namespace Taschenrechner
         {
             RechnerModel model = new RechnerModel();            
             ConsoleView view = new ConsoleView(model);
+            AnwendungsController controller = new AnwendungsController(view, model);
 
-            // Eingabe
-            string ersteZahlAlsString = view.HoleZahlVomBenutzer();
-            string rechenoperator = view.HoleOperatorVomBenutzer();
-            string zweiteZahlAlsString = view.HoleZahlVomBenutzer();
-            
-            // Wandel Text in Gleitkommazahlen
-            // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist
-            double ersteZahl = Convert.ToDouble(ersteZahlAlsString);
-            double zweiteZahl = Convert.ToDouble(zweiteZahlAlsString);
+            controller.Ausfuehren();
 
-            // Berechnung ausführen
-            model.Berechne(ersteZahl, zweiteZahl, rechenoperator);
-
-            // Ausgabe
-            view.GibResultatAus(zweiteZahl, rechenoperator);
-            Console.WriteLine();
-            view.WarteAufEndeDurchBenutzer();
         }
 
 
